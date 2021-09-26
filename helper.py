d9b0521e7097944e5739fa2039be2ea60a8fe34d
gursi26 <gursimar.batra@gmail.com>
2021-09-26 13:08:06 +0800

    mail movement setup

---
+++ helper.py
@@ -0,0 +1,57 @@
+import re 
+
+class SpamDetection:
+
+    def __init__(self):
+        self.nonos = ['http', '<', '{', '(', '\\', '[', ')', '[']
+
+    def clean_text(self, text):
+        text = re.sub('\W+',' ',text).strip()
+        text = text.replace('\n', '')
+        text = text.replace('\r', ' ')
+        subs = text.split(' ')
+        subs = list(filter(('').__ne__, subs))
+        
+        to_remove = []
+        for token in subs : 
+            flag = True 
+            for nono in self.nonos : 
+                if nono in token : 
+                    flag = False 
+                    break 
+            if not flag : 
+                to_remove.append(token)
+
+        output = [i for i in subs if i not in to_remove]
+        text = ' '.join(output)
+        return text 
+
+    def classify(self, body, sender, subject, print_body=False):
+        cleaned_body = self.clean_text(body)
+        if print_body : 
+            print(f'Body: {cleaned_body}\n')
+        # pass through model 
+        return False 
+
+
+def mark_unseen(imap, mail_uid):
+    r = imap.store(mail_uid, '-FLAGS', '\Seen')
+    if r[0] == 'OK':
+        print('Mail marked as unseen.')
+
+def relabel_and_delete(imap, mail_uid, spam_folder_name):
+    r1 = imap.store(mail_uid, '+X-GM-LABELS', spam_folder_name)
+    mark_unseen(imap, mail_uid)
+    if r1[0] == 'OK':
+        print('Mail relabeled.')
+        r3 = imap.store(mail_uid, "+FLAGS", "\\Deleted")
+        if r3[0] == 'OK' : 
+            print('Mail deleted.')
+
+def sort_email(imap, spam, mail_uid, spam_folder_name):
+    if spam : 
+        print('Spam detected.')
+        relabel_and_delete(imap, mail_uid, spam_folder_name)
+    else : 
+        print('No spam detected.')
+        mark_unseen(imap, mail_uid)
